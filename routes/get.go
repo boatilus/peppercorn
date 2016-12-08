89676c4bf2133180c9ca5df13e06b04ec8cf2245
Ron Jones <r@ovao.la>
2016-12-08 07:25:27 -0800

    Refactor routes slightly

--- routes/get.go
+++ routes/get.go
@@ -2,19 +2,20 @@ package routes
 
 import (
 	"fmt"
-	"github.com/boatilus/peppercorn/posts"
-	"github.com/boatilus/peppercorn/users"
-	"github.com/gorilla/mux"
 	"io"
 	"net/http"
 	"strconv"
+
+	"github.com/boatilus/peppercorn/posts"
+	"github.com/boatilus/peppercorn/users"
+	"github.com/gorilla/mux"
 )
 
+// IndexHandler is called for the `/` (index) route and
 func IndexHandler(writer http.ResponseWriter, _ *http.Request) {
 	//user, err := GetUserByID("de0dc022-e1d7-4985-ba53-0b4579ada365")
 	//user, err := GetUserByName("boat")
 	ps, err := posts.GetRange(1, 10)
-
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
@@ -26,10 +27,9 @@ func IndexHandler(writer http.ResponseWriter, _ *http.Request) {
 func PageHandler(writer http.ResponseWriter, req *http.Request) {
 	num := mux.Vars(req)["num"]
 
-	page_num, parse_err := strconv.ParseUint(num, 10, 64)
-
-	if parse_err != nil {
-		msg := fmt.Sprintf("Bad request: for route \"/page/%v\", expected \"%v\" to be a positive integer", num, num)
+	n, err := strconv.ParseUint(num, 10, 64)
+	if err != nil {
+		msg := fmt.Sprintf("Bad request for route 'page/%v'. Expected '%v' to be a positive integer", num, num)
 
 		http.Error(writer, msg, http.StatusBadRequest)
 
@@ -38,7 +38,6 @@ func PageHandler(writer http.ResponseWriter, req *http.Request) {
 
 	// TODO: Replace with session data
 	user, err := users.GetByID("9b00b4c6-fdcd-44f3-b797-fe009ddd9042")
-
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusForbidden)
 
@@ -49,12 +48,10 @@ func PageHandler(writer http.ResponseWriter, req *http.Request) {
 		user.PPP = 10
 	}
 
-	start := (page_num * uint64(user.PPP)) - uint64(user.PPP) + 1
-
-	ps, geterr := posts.GetRange(start, uint64(user.PPP))
-
-	if geterr != nil {
-		http.Error(writer, geterr.Error(), http.StatusNotFound)
+	start := (n * uint64(user.PPP)) - uint64(user.PPP) + 1
+	ps, err := posts.GetRange(start, uint64(user.PPP))
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusNotFound)
 
 		return
 	}
@@ -66,21 +63,21 @@ func PageHandler(writer http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// SingleHandler is called for GET requests for the `/post/{num}` route and renders a single post
+// by its computed post number.
 func SingleHandler(writer http.ResponseWriter, req *http.Request) {
 	num := mux.Vars(req)["num"]
 
-	post_num, parse_err := strconv.ParseUint(num, 10, 64)
-
-	if parse_err != nil {
-		msg := fmt.Sprintf("Bad request: for route \"/post/%v\", expected \"%v\" to be a positive integer", num, num)
+	n, err := strconv.ParseUint(num, 10, 64)
+	if err != nil {
+		msg := fmt.Sprintf("Bad request for route '/post/%v'. Expected '%v' to be a positive integer", num, num)
 
 		http.Error(writer, msg, http.StatusBadRequest)
 
 		return
 	}
 
-	p, err := posts.GetOne(post_num)
-
+	p, err := posts.GetOne(n)
 	if err != nil {
 		http.NotFound(writer, req)
 	}
