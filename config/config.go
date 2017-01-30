4abab459424fae9f3fcd4b91ab786750fd30dbb1
Ron Jones <r@ovao.la>
2017-01-29 21:15:11 -0800

    Add config package

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+const configName = "config"
+const configPath = "."
+
+func init() {
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configPath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		// We needn't panic or anything here, since we'll create the config with functional defaults.
+		log.Print("config: Could not read config file")
+	}
+
+	// Merely return and skip configuring the Sentry hook if no Sentry DSN specified in the config.
+	dsn := viper.GetString("sentry.dsn")
+	if dsn == "" {
+		return
+	}
+}
+
+func GetString(key string) string {
+	return viper.GetString(key)
+}
