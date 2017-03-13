efc66f8a620e577eae74662f9d224476f116e6da
Ron Jones <r@ovao.la>
2017-03-12 18:26:29 -0700

    enable 2FA: add TOTP generation logic to route

--- routes/enable-two-factor-authentication.go
+++ routes/enable-two-factor-authentication.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"bytes"
+	"encoding/base64"
+	"image/png"
 	"net/http"
 
 	"github.com/boatilus/peppercorn/templates"
 	"github.com/boatilus/peppercorn/users"
+	"github.com/pquerna/otp/totp"
 )
 
 func EnableTwoFactorAuthentication(w http.ResponseWriter, req *http.Request) {
@@ -19,5 +23,49 @@ func EnableTwoFactorAuthentication(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	templates.EnableTwoFactorAuthentication.Execute(w, nil)
+	// We need to bail out if the user's already enabled 2FA, which we'll do with an error.
+	if u.Has2FAEnabled {
+		http.Error(w, "EnableTwoFactorAuthentication: 2FA already enabled", http.StatusBadRequest)
+		return
+	}
+
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "peppercorn",
+		AccountName: u.ID,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var buf bytes.Buffer
+	img, err := key.Image(500, 500)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := png.Encode(&buf, img); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	base64Image := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+	type data struct {
+		QRCode string
+		Secret string
+	}
+
+	u.TOTPSecret = key.Secret()
+
+	if err := users.Update(u); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	templates.EnableTwoFactorAuthentication.Execute(w, data{
+		base64Image,
+		key.Secret(),
+	})
 }
